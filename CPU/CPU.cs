fc986e874378716bbe6f8deceae4bfddc9d6335f
elias <elkkuulkku@gmail.com>
2021-02-10 16:22:33 +0200

    Created project

---
+++ CPU/CPU.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CPU
+{
+    class Cpu
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void LoadData()
+        {
+            Console.WriteLine("Loading data");
+        }
+    }
+}
