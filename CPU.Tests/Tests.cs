34767039ade4be4ec6c724937220750325881ada
Elias Mäkelä <eliasjmakela@protonmail.com>
2021-04-26 21:27:55 +0300

    Added test for ParityCounter

--- CPU.Tests/Tests.cs
+++ CPU.Tests/Tests.cs
@@ -15,10 +15,10 @@ namespace CPU.Tests
             Instructions.Add(cpu, Register.B);
 
             //Check that the value in the accumulator is correct
-            Assert.True(cpu.Registers[Register.A] == 5);
+            Assert.That(cpu.Registers[Register.A], Is.EqualTo(5));
 
             //Check that the carry flag is unset as it should be
-            Assert.True((cpu.Flags & FlagSelector.Carry) == 0);
+            Assert.That(cpu.Flags & FlagSelector.Carry, Is.Zero);
         }
 
         [Test]
@@ -29,7 +29,7 @@ namespace CPU.Tests
             BitHelper.SetFlag(1, FlagSelector.Carry, cpu);
 
             //Check that carry is set
-            Assert.True(cpu.Flags == FlagSelector.Carry);
+            Assert.That(cpu.Flags, Is.EqualTo(FlagSelector.Carry));
         }
 
         [Test]
@@ -38,7 +38,7 @@ namespace CPU.Tests
             var selector = BitHelper.FlagConstructor("PA");
 
             //check that constructed selector is correct
-            Assert.True(selector == (FlagSelector.Parity | FlagSelector.AuxCarry));
+            Assert.That(selector, Is.EqualTo(FlagSelector.Parity | FlagSelector.AuxCarry));
         }
         
         
@@ -50,13 +50,22 @@ namespace CPU.Tests
             BitHelper.SetFlag(0, FlagSelector.Parity, cpu);
 
             //Check that sign is set
-            Assert.True(cpu.Flags == FlagSelector.Sign);
+            Assert.That(cpu.Flags, Is.EqualTo(FlagSelector.Sign));
 
             //Check that rest are unset
-            Assert.True((cpu.Flags & BitHelper.FlagConstructor("CPAZ")) == 0);
-
-
-
+            Assert.That((cpu.Flags & BitHelper.FlagConstructor("CPAZ")), Is.Zero);
+        }
+        [Test]
+        public void BitHelperParityCounter_ParityUneven_Return0()
+        {
+            var parity = BitHelper.ParityCounter(0b00011111);
+            Assert.That(parity, Is.EqualTo(0));
+        }
+        [Test]
+        public void BitHelperParityCounter_ParityEven_Return1()
+        {
+            var parity = BitHelper.ParityCounter(0b00001111);
+            Assert.That(parity, Is.EqualTo(1));
         }
     }
 }
